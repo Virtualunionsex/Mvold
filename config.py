8295b18c3a883297eaf27cf7b88a62e1e9a2e02c
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2024-04-10 13:30:36 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,9 @@ db_url = os.environ.get("DB_URL", "mongodb+srv://Menfess:Malik10_@cluster0.0q7u1
 db_name = os.environ.get("DB_NAME", "Cluster0")
 # =========================================================== #
 
-channel_1 = int(os.environ.get("CHANNEL_1", "-1002063775407"))
+channel_1 = int(os.environ.get("CHANNEL_1", "-1002024649219"))
 channel_2 = int(os.environ.get("CHANNEL_2", "-1001842994504"))
+channel_3 = int(os.environ.get("CHANNEL_3", "-1001842994504"))
 channel_log = int(os.environ.get("CHANNEL_LOG", "-1001628283862"))
 # =========================================================== #
 
